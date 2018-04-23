5eb7bff73ded4fc3c9c22d87a0033e12a98e163a
Brian McGinnis <brian@nobox.io>
2018-04-23 16:28:39 -0500

    Removed a thing

--- Main.py
+++ Main.py
@@ -99,7 +99,7 @@ def main():
     say = vr.viewFaces()
     tts.speak(say, False)
 
-    bioloid.doIdle(False)
+    #bioloid.doIdle(False)
     # replace with robot name
     name = config.get('Bioloid Information','name')
 
